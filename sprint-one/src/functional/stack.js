cb82e0205d4cb2fde2eaf73684b7d5f8652d6f5b
akwong372 <akwong372@gmail.com>
2018-10-06 12:12:57 -0700

    Allows sequentially adding and removing items - removes the most recently added of two items

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,20 +4,24 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
+  var lastValue;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    lastValue = storage[size];
     size++;
   };
 
   someInstance.pop = function() {
-    size--;
+    if (size > 0){
+      delete storage[size];
+      size--;
+    }
+    return lastValue;
   };
 
   someInstance.size = function() {
-    if (size <= 0){
-      return 0;
-    }
     return size;
   };
 
