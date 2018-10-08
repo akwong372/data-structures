614c199437ea798f4f2ef68956de722438417dac
akwong372 <akwong372@gmail.com>
2018-10-08 13:18:20 -0700

    reports a size of 0 for a new stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = {};
+  newInstance.storage = {};
+  newInstance.storageSize = 0;
+
+  _.extend(newInstance, stackMethods);
+
+  return newInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+
+};
+
+stackMethods.pop = function(value){
+
+};
+
+stackMethods.size = function(value){
+  return this.storageSize;
+};
 
