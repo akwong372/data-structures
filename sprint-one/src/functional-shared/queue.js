9c5aaf66ade2f8fbfc40dc13eb81d3868f5ee4da
akwong372 <akwong372@gmail.com>
2018-10-08 20:59:21 -0700

    Allows sequentially removing/adding items - removes the least recently added of two items

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -14,13 +14,18 @@ var Queue = function() {
 var queueMethods = {};
 
 queueMethods.enqueue = function(value) {
+  this.storage[this.storageSize] = value;
+  this.lastItem = value;
   this.storageSize++;
 };
 
 queueMethods.dequeue = function() {
   if (this.storageSize > 0){
     this.storageSize--;
+    this.firstItem = this.storage[0];
+    delete this.storage[0];
   }
+  return this.firstItem;
 
 };
 
