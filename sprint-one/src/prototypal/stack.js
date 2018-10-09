e290aae3d3baf7b431b06f2b0391702885d89c2d
akwong372 <akwong372@gmail.com>
2018-10-08 21:55:40 -0700

    Reports size of stack - reports 0 for a new stack - reports 0 when more items removed than added

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var newInstance = Object.create(stackMethods);
+newInstance.storage = {};
+newInstance.storageSize = 0;
+
+return newInstance;
 };
 
-var stackMethods = {};
+stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storageSize++;
+}
 
+stackMethods.pop = function() {
+  if (this.storageSize > 0){
+    this.storageSize--;
+  }
+}
+
+stackMethods.size = function() {
+  return this.storageSize;
+}
