3146125ebbdbcfd55d93c5808dea9d813efae937
akwong372 <akwong372@gmail.com>
2018-10-09 12:51:53 -0700

    Reports queue size after adding/removing items - Reports size 0 for new queue - allows sequentially adding/removing items

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.storageSize = 0;
+  this.lastItem;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.storageSize] = value;
+  this.storageSize++;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.storageSize > 0){
+    this.storageSize--;
+    this.lastItem = this.storage[this.storageSize]
+    delete this.storage[this.storageSize];
+  }
+  return this.lastItem;
+};
+
+Queue.prototype.size = function() {
+  return this.storageSize;
+};
+
+var newInstance = new Queue();
