f2e63c7b9161c0b00f0db9adc0dbc5497dd1b130
akwong372 <akwong372@gmail.com>
2018-10-10 21:37:30 -0700

    Add designates new tail when new nodes added

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,14 +1,29 @@
 var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
-  //something
-  // more stuff
+  var list = {}; // {head: value, tail: value{value: 12, next{value: 99, next: {} }}
+  list.head = null; // {value: 12, next{value: 99, next: {}}
+  list.tail = null; // { value: 10, next: {}}
 
-  list.addToTail = function(value) {
+
+  list.addToTail = function(value){
+  // if list is empty
+    // list.head = new Node(value)
+
+    // if(Object.keys(list).length === 0) {
+    //   list.head = new Node(value);
+    //   list.tail = list.head;
+    // } else {
+    //   // have the previous next point to the new node;
+    //     // current tail points to the new thing
+    //       // new thing becomes the next tail
+    //   list.tail.next = new Node(value);
+    //   list.tail = list.tail.next;
+    // }
+    list.tail = new Node(value);
   };
 
   list.removeHead = function() {
+    //it should point to the next value after the first head is removed
+    list.head = list.head.next;
   };
 
   list.contains = function(target) {
@@ -20,8 +35,8 @@ var LinkedList = function() {
 var Node = function(value) {
   var node = {};
 
-  node.value = value;
-  node.next = null;
+  node.value = value; // 1
+  node.next = null; //list.tail.next
 
   return node;
 };
