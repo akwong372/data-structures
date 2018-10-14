702b8d475b63dfa7b5e70d21995558c19e4b7891
akwong372 <akwong372@gmail.com>
2018-10-13 17:55:56 -0700

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,50 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me{value: 5, children:[]}
+                          //{value: "1", children: [{value: 5, children: []}, {value:6, children:[]}]}
+
+  extend(newTree, treeMethods);
 
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // value should be passed in
+  // 'this' will refer to the tree that we want to look at.
+  this.children.push(Tree(value));
+
+  // this.children.push({
+  //   'value' : value,
+  //   'children' : []
+  // });
+
 };
+
+// var myTree = {value: undefined, children: [{value: 6 , children: [{value: 5, children: []}]}]}
 
 treeMethods.contains = function(target) {
+  var branch = this;
+// var traverse = function(branch, target){
+    if (branch.value === target) { // false
+      return true;
+    }
+
+  if (branch.children.length > 0) {
+    for (var i = 0; i < branch.children.length; i++) {
+      if (branch.children[i].contains(target))
+        return true;
+    }
+  }
+
+return false;
 };
 
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
