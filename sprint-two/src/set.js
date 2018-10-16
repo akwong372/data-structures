bc412c6e00a0b4190b0bf7433c1b35d1a3eeea2c
PTSoup <phaimani.souphaphone@gmail.com>
2018-10-15 23:56:51 -0500

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return Boolean(this._storage[item]);
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ // The time complexity would be 01, because all of the methods/functions are direct lookups.
+ // Alvin and TJ.
  */
