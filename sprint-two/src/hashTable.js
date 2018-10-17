5218d662beac1c75b2b6585fbc119f2a7dc6b9cc
akwong372 <akwong372@gmail.com>
2018-10-16 22:11:30 -0700

    Completed hashTable up to collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,23 @@
 
 
-var HashTable = function() {
+var HashTable = function() { //steven seagal, [[steven, seagal]]
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function(k, v) { //
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
